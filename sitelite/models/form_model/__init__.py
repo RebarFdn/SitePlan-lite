4b5059aa80103c18fcc7f2b7fdde589587db359b
CentryPlan <rebar.conn@gmail.com>
2025-04-28 00:42:04 -0500

    modified:   pyproject.toml 	modified:   sitelite/models/form_model/__init__.py 	new file:   sitelite/models/form_model/forms.py 	new file:   sitelite/models/form_model/interfaces.py 	new file:   sitelite/models/form_model/objects.py 	new file:   sitelite/models/form_model/services.py 	new file:   sitelite/models/form_model/strategies.py 	modified:   sitelite/routes/appHome.py 	modified:   static/css/site.css 	modified:   templates/baseTemplates/main_template.html 	modified:   templates/index.html 	new file:   templates/user_form.html 	modified:   uv.lock

--- sitelite/models/form_model/__init__.py
+++ sitelite/models/form_model/__init__.py
@@ -1,23 +1,51 @@
 from typing import Any
-from pydantic import BaseModel, EmailStr, Field, ConfigDict, ValidationError
-from starlette.responses import StreamingResponse
+from pydantic import BaseModel, EmailStr, Field, ConfigDict, ValidationError ,field_validator
+from starlette.responses import StreamingResponse,  HTMLResponse
+
 
 
 class ModelForm(BaseModel):     
     model_config = ConfigDict(json_schema_extra={'icon': 'location-arrow'})  
+
     
-    def form(self, insert:bool=False):
-        """Generate a form for the Location model
+    def form_template(self, insert:bool=False):
+        """Returns a Jinja templated html form of the model 
+
+        Args:
+            insert (bool, optional): to insert css and icons resources or use local resources.
+        """
+        form_html:str =""
+        for i in  self.generate_html_form(insert=insert):
+            form_html = form_html + i
+        return  form_html
+    
+
+    def html_form(self, insert:bool=False):
+        """Returns a html form of the model 
+
+        Args:
+            insert (bool, optional): to insert css and icons resources or use local resources.
+        """
+        form_html:str =""
+        for i in  self.generate_html_form(insert=insert):
+            form_html = form_html + i
+        return  HTMLResponse(form_html)
+
+    
+    def stream_html_form(self, insert:bool=False):
+        """Streams the Generated html form for the model
         Args:
             request (Request): The request object
         Returns:
             HTMLResponse: The HTML response with the form
         """
-        print("Generating form")
-        
-        def generate_form():
-            """Generate a form for the Location model"""
-            #print(self.model_json_schema())
+        print("Streaming form")
+
+        return StreamingResponse( self.generate_html_form(insert=insert), media_type="text/html")
+
+
+    def generate_html_form(self, insert=False):
+            """Generates a Html form of the instantiated model"""            
             if not insert:
                 yield """<!DOCTYPE html><html lang="en">
                 <head>
@@ -45,135 +73,22 @@ class ModelForm(BaseModel):
                     <label class="label" for="{key}">{value.get('title')}</label>"""
                     if value.get('type') == 'number':
                         yield f"""<div class="control has-icons-left">
-                        <input class="input is_primary" type="{value.get('type')}" step="0.001" name="{key}" id="{key}" placeholder="{value.get('title')}" />
+                        <input class="input is_primary" type="{value.get('type')}" step="0.001" name="{key}" id="{key}" placeholder="{value.get('title')}" value="{{form.fields[{key}].value}}" />
                          <span class="icon is-small is-left">
                             <i class="fas fa-{value.get('icon')}"></i>
                         </span>                      
-                        
-                        </div>                        
-                        """
-                    if key == 'password':
-                        yield f"""<div class="control has-icons-left">
-                        <input class="input is_primary" type="{key}" name="{key}" id="{key}" placeholder="{value.get('title')}" />
-                         <span class="icon is-small is-left">
-                            <i class="fas fa-{value.get('icon')}"></i>
-                        </span>                      
-                        
-                        </div>                        
-                        """
-
-                        ## if value.validation_error:
-                            # yield f""" <span class="is-size-7 has-text-danger has-text-weight-bold">{value.get(errors)[0]}</span>
-                    # do further checks for file upload field, radio buttons, checkboxes,select fields etc...
-                    else:
-                        yield f"""<div class="control has-icons-left"> <input class="input is_primary" type="{value.get('type')}" name="{key}" id="{key}" placeholder="{value.get('title')}" />
-                        <span class="icon is-small is-left">
-                            <i class="fas fa-{value.get('icon')}"></i>
-                        </span>                      
-                        
-                        </div>"""
-            # process the nested fields
-            if self.model_json_schema().get('$defs'):
-                yield f"""<div class="collapse bg-base-100 border border-base-300">
-                <input type="radio" name="my-accordion-1" checked="checked" />"""
-                for key2, value2 in self.model_json_schema().get('$defs').items():
-                    yield f"""<div class="collapse-title font-semibold"> {key2}</div>
-                            <div class="collapse-content text-sm">"""                    
-                    for key3, value3 in value2.get('properties').items():
-                        yield f"""<div class="field">        
-                            <label class="label" for="{key3}">{value3.get('title')}</label>"""
-                        if value3.get('type') == 'number':
-                            yield f"""<div class="control has-icons-left"> <input  class="input is_primary" type="{value3.get('type')}" step="0.001" name="{key3}" id="{key3}" placeholder="{value3.get('title')}" /><span class="icon is-small is-left">
-                            <i class="fas fa-{value3.get('icon')}"></i>
-                        </span>                      
-                        
-                            </div>"""
-                        # do further checks for file upload field, radio buttons, checkboxes,select fields etc...
-                        else:
-                            yield f"""<div class="control has-icons-left"> <input  class="input is_primary" type="{value3.get('type')}" name="{key3}" id="{key3}" placeholder="{value3.get('title')}" /><span class="icon is-small is-left">
-                            <i class="fas fa-{value3.get('icon')}"></i>
-                        </span></div>
-                                   
-                            """
-                    yield """</div>"""
-                yield """</div>"""                 
-                    
-            yield """<div class="field is-grouped mt-5">
-                    <div class="control has-icons-left">
-                        <input type="submit" button class="button is-link" value="Submit"></input>
-                    </div>
-                    <div class="control has-icons-left">
-                        <button class="button is-link is-light">Cancel</button>
-                    </div></div></form></div>"""
-            if not insert: 
-                yield """</body></html>"""
-        return StreamingResponse( generate_form(), media_type="text/html")
-
-    def accordion():
-        return """
-                <div class="collapse bg-base-100 border border-base-300">
-                <input type="radio" name="my-accordion-1" checked="checked" />
-                <div class="collapse-title font-semibold">How do I create an account?</div>
-                <div class="collapse-content text-sm">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
-                </div>
-                <div class="collapse bg-base-100 border border-base-300">
-                <input type="radio" name="my-accordion-1" />
-                <div class="collapse-title font-semibold">I forgot my password. What should I do?</div>
-                <div class="collapse-content text-sm">Click on "Forgot Password" on the login page and follow the instructions sent to your email.</div>
-                </div>
-                <div class="collapse bg-base-100 border border-base-300">
-                <input type="radio" name="my-accordion-1" />
-                <div class="collapse-title font-semibold">How do I update my profile information?</div>
-                <div class="collapse-content text-sm">Go to "My Account" settings and select "Edit Profile" to make changes.</div>
-                </div>
-        """
-    
-    def generate_form(self, insert=False):
-            """Generate a form for the Location model"""
-            #print(self.model_json_schema())
-            if not insert:
-                yield """<!DOCTYPE html><html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>{{ title }} </title>
-                    <link rel="stylesheet" href="/static/jscss/fontawesome-free-6.7.2-web/css/fontawesome.css" />
-                    <link rel="stylesheet" href="/static/jscss/fontawesome-free-6.7.2-web/css/brands.css" />
-                    <link rel="stylesheet" href="/static/jscss/fontawesome-free-6.7.2-web/css/solid.css" />
-                    <link rel="stylesheet" href="/static/jscss/fontawesome-free-6.7.2-web/css/svg-with-js.css" />
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
-                    <link rel="stylesheet" type="text/css" href="/static/site.css">
-                     
-                </head>
-                <body>"""
-            yield """<div style="margin:50px;"><form method="POST">                
-                <input type="hidden" name="csrf" value="{{form.csrf}}" />"""
-            yield f""" <h3 class="title is-4">{ self.model_json_schema().get('title')}</h3> """
-            
-            for key, value in self.model_json_schema().get('properties').items():                
-                if value.get('$ref'):
-                    pass
-                else:
-                    yield f""" <div class="field">    
-                    <label class="label" for="{key}">{value.get('title')}</label>"""
-                    if value.get('type') == 'number':
-                        yield f"""<div class="control has-icons-left">
-                        <input class="input is_primary" type="{value.get('type')}" step="0.001" name="{key}" id="{key}" value="{getattr(self, key)}" />
-                         <span class="icon is-small is-left">
-                            <i class="fas fa-{value.get('icon')}"></i>
-                        </span>                      
-                        
+                         <div id="error"></div>
                         </div>                        
                         """
                         ## if value.validation_error:
                             # yield f""" <span class="is-size-7 has-text-danger has-text-weight-bold">{value.get(errors)[0]}</span>
                     # do further checks for file upload field, radio buttons, checkboxes,select fields etc...
                     else:
-                        yield f"""<div class="control has-icons-left"> <input class="input is_primary" type="{value.get('type')}" name="{key}" id="{key}" value="{getattr(self, key)}" />
+                        yield f"""<div class="control has-icons-left"> <input class="input is_primary" type="{value.get('type')}" name="{key}" id="{key}" placeholder="{value.get('title')}" value="{{form.fields[{key}].value}}" />
                         <span class="icon is-small is-left">
                             <i class="fas fa-{value.get('icon')}"></i>
                         </span>                      
-                        
+                         <div id="error"></div>
                         </div>"""
             # process the nested fields
             if self.model_json_schema().get('$defs'):
@@ -185,16 +100,21 @@ class ModelForm(BaseModel):
                         yield f"""<div class="field">        
                             <label class="label" for="{key3}">{value3.get('title')}</label>"""
                         if value3.get('type') == 'number':
-                            yield f"""<div class="control has-icons-left"> <input  class="input is_primary" type="{value3.get('type')}" step="0.001" name="{key3}" id="{key3}" placeholder="{value3.get('title')}" /><span class="icon is-small is-left">
+                            yield f"""<div class="control has-icons-left"> <input  class="input is_primary" type="{value3.get('type')}" step="0.001" name="{key3}" id="{key3}" placeholder="{value3.get('title')}" value="{{form.fields[{key3}].value}}" />
+                            <span class="icon is-small is-left">
                             <i class="fas fa-{value3.get('icon')}"></i>
-                        </span>                      
-                        
+                            </span>                      
+                             <div>{{ form.fields.[{key3}].error }}</div>
                             </div>"""
                         # do further checks for file upload field, radio buttons, checkboxes,select fields etc...
                         else:
-                            yield f"""<div class="control has-icons-left"> <input  class="input is_primary" type="{value3.get('type')}" name="{key3}" id="{key3}" placeholder="{value3.get('title')}" /><span class="icon is-small is-left">
+                            yield f"""<div class="control has-icons-left"> 
+                            <input  class="input is_primary" type="{value3.get('type')}" name="{key3}" id="{key3}" placeholder="{value3.get('title')}" value="{{form.fields[{key3}].value}}"/>
+                            <span class="icon is-small is-left">
                             <i class="fas fa-{value3.get('icon')}"></i>
-                        </span></div>
+                            </span>
+                             <div>{{ form.fields.[{key3}].error }}</div>
+                            </div>
                                    
                             """
                     yield """</div></li>"""
@@ -209,4 +129,25 @@ class ModelForm(BaseModel):
                     </div></div></form></div>"""
             if not insert: 
                 yield """</body></html>"""
-            return None
+            
+
+if __name__ == '__main__':
+    
+    class MyForm(ModelForm):
+        name: str = Field(default=None, min_length=1)
+        age: int = None
+
+        @field_validator('age')
+        @classmethod
+        def age_must_be_over_30(cls, value, values):
+            if value <=20:
+                raise ValueError("30's are your new 20's")
+            return value
+        
+    mf:dict = {'name': "Anuk Amen-RA", 'age':23} 
+    try:
+        model = MyForm() #.model_validate(mf)
+        print(model.form_template())
+        
+    except ValidationError as ve:
+        print(ve)
--- sitelite/routes/appHome.py
+++ sitelite/routes/appHome.py
@@ -2,10 +2,25 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, JSONResponse
 from config import TEMPLATES
 from starlette.routing import Mount,Route, WebSocketRoute
-from pydantic import ValidationError, PydanticUserError
+from pydantic import BaseModel, Field, ValidationError,field_validator
 from models.auth_models import Password, RegisterUser
+from models.form_model import ModelForm
 
 # Application Home Page
+
+class MyForm(ModelForm):
+    name: str = Field(default=None, min_length=1)
+    age: int = None
+
+    @field_validator('age')
+    @classmethod
+    def age_must_be_over_30(cls, value, values):
+        if value <=30:
+            raise ValueError("30's are your new 20's")
+        return value
+
+
+
 
 
 async def HomePage(request:Request):
@@ -15,20 +30,24 @@ async def HomePage(request:Request):
 async def Register(request:Request):
     '''  '''
     if request.method == 'POST':
-        form = await request.form()
-        try:
-            data = RegisterUser( 
-                name=form.get('name'),
-                username=form.get('username'), 
-                email=form.get('email'), 
-                password=form.get('password') 
-            )
-            return HTMLResponse(f"""<div>{data.model_dump()}</div>""")
-        except  ValidationError as ero:           
-            return HTMLResponse(f"""<div>{ero}</div>""")       
+        form = await request.form() 
+        if MyForm.model_validate_json(form):
+            model = MyForm( **form )
+            return HTMLResponse(f"<p>Hello {model.name} you are { model.age } years old.</p>")
+        else:
+            return TEMPLATES.TemplateResponse('user_form.html', {"request":request, 'form': form})
+        """
+                data = RegisterUser( 
+                    name=form.get('name'),
+                    username=form.get('username'), 
+                    email=form.get('email'), 
+                    password=form.get('password') 
+                )
+        """
         
     else:
-        form = RegisterUser().form()
+        #dataForm = await PydanticForm.create(request, MyForm)
+        form = MyForm().html_form()
         return form
 
 
